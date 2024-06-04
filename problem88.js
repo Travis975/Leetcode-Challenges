aa78baa3a644b0e790292c16533a29928bb07ab7
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-06-04 11:54:47 -0700

    Challenge #3

---
+++ problem88.js
@@ -0,0 +1,40 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+// To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+// and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    // Pointers for nums1, nums2, and the merged array
+    // Last element in the initial part of nums1
+    let p1 = m - 1;
+    // Last element in nums2
+    let p2 = n - 1; 
+    // Last position in nums1
+    let pMerged = m + n - 1;
+    // Merge nums1 and nums2 from the back to the front
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[pMerged] = nums1[p1];
+            p1--;
+        } else {
+            nums1[pMerged] = nums2[p2];
+            p2--;
+        }
+        pMerged--;
+    }
+
+    // If there are any remaining elements in nums2, copy them
+    while (p2 >= 0) {
+        nums1[pMerged] = nums2[p2];
+        p2--;
+        pMerged--;
+    }
+};
---
+++ problem912.js
@@ -0,0 +1,31 @@
+// Given an array of integers nums, sort the array in ascending order and return it.
+// You must solve the problem without using any built-in functions in O(nlog(n)) time 
+// complexity and with the smallest space complexity possible
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    if (nums.length <= 1) return nums;
+
+    const middle = Math.floor(nums.length / 2);
+    const left = nums.slice(0, middle);
+    const right = nums.slice(middle);
+
+    return merge(sortArray(left), sortArray(right));
+
+};
+function merge(left, right) {
+    let sortedArr = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        }
+        else{
+            sortedArr.push(right.shift());
+        }
+    }
+    return [...sortedArr, ...left, ...right]
+}

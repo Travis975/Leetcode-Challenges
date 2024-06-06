34851173677b1169fdd099cd680c42c82d5c9c26
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-06-06 13:29:11 -0700

    Challenge #3

--- problem88.js
+++ problem88.js
@@ -1,4 +1,5 @@
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+// representing the number of elements in nums1 and nums2 respectively.
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
 // To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
@@ -11,6 +12,7 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
 var merge = function(nums1, m, nums2, n) {
     // Pointers for nums1, nums2, and the merged array
     // Last element in the initial part of nums1
--- problem912.js
+++ problem912.js
@@ -2,10 +2,11 @@
 // You must solve the problem without using any built-in functions in O(nlog(n)) time 
 // complexity and with the smallest space complexity possible
 
-/**
+/*
  * @param {number[]} nums
  * @return {number[]}
  */
+
 var sortArray = function(nums) {
     if (nums.length <= 1) return nums;
 
